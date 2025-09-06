fee1efa19c832ea84966beb3770cbedfbf63d2cd
SANTOSH_KUMAR <103521314+santosh226206@users.noreply.github.com>
2025-09-06 21:27:27 +0530

    sdbesr (#2)

---
+++ src/main/java/linkedList/ListNodeDelTest.java
@@ -0,0 +1,13 @@
+package linkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListNodeDelTest {
+    public static void main(String[] args) {
+        List<Integer> lt =new LinkedList<>();
+        lt.add(2);
+        lt.add(3);
+        System.out.println(lt.toString());
+    }
+}
